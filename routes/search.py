4788342113c0e7b4368cac459dc66fd45f3486b7
Chao <chaoxu18@vt.edu>
2019-09-17 15:19:41 -0400

    remove:

--- routes/search.py
+++ routes/search.py
@@ -1,25 +1,23 @@
-from flask import Blueprint,render_template,request,jsonify
-import requests,json
+from flask import Blueprint, render_template, request, jsonify
+import requests, json
 
 # creating a Blueprint class
-search_blueprint = Blueprint('search',__name__,template_folder="templates")
+search_blueprint = Blueprint("search", __name__, template_folder="templates")
 search_term = ""
 
 
-headers = {
-    'Content-Type': "application/json",
-    'cache-control': "no-cache",
-}
+headers = {"Content-Type": "application/json", "cache-control": "no-cache"}
 
-@search_blueprint.route("/",methods=['GET','POST'],endpoint='index')
+host = "localhost:9200"
+
+
+@search_blueprint.route("/", methods=["GET", "POST"], endpoint="index")
 def index():
-    if request.method=='GET':
-        res ={
-	            'hits': {'total': 0, 'hits': []}
-             }
-        return render_template("index.html",res=res)
-    elif request.method =='POST':
-        if request.method == 'POST':
+    if request.method == "GET":
+        res = {"hits": {"total": 0, "hits": []}}
+        return render_template("index.html", res=res)
+    elif request.method == "POST":
+        if request.method == "POST":
             print("-----------------Calling search Result----------")
             search_term = request.form["input"]
             print("Search Term:", search_term)
@@ -28,41 +26,31 @@ def index():
                     "query_string": {
                         "analyze_wildcard": True,
                         "query": str(search_term),
-                        "fields": ["topic", "title", "url", "labels"]
+                        "fields": ["topic", "title", "url", "labels"],
                     }
                 },
                 "size": 50,
-                "sort": [
-
-                ]
+                "sort": [],
             }
             payload = json.dumps(payload)
-            url = "http://elasticsearch:9200/hacker/tutorials/_search"
+            url = "http://{}/hacker/tutorials/_search".format(host)
             response = requests.request("GET", url, data=payload, headers=headers)
             response_dict_data = json.loads(str(response.text))
-            return render_template('index.html', res=response_dict_data)
+            return render_template("index.html", res=response_dict_data)
 
 
-@search_blueprint.route("/autocomplete",methods=['POST'],endpoint='autocomplete')
+@search_blueprint.route("/autocomplete", methods=["POST"], endpoint="autocomplete")
 def autocomplete():
-    if request.method == 'POST':
+    if request.method == "POST":
         search_term = request.form["input"]
         print("POST request called")
         print(search_term)
-        payload ={
-          "autocomplete" : {
-            "text" : str(search_term),
-            "completion" : {
-              "field" : "title_suggest"
-            }
-          }
+        payload = {
+            "autocomplete": {"text": str(search_term), "completion": {"field": "title_suggest"}}
         }
         payload = json.dumps(payload)
-        url="http://elasticsearch:9200/autocomplete/_suggest"
+        url = "http://{}/autocomplete/_suggest".format(host)
         response = requests.request("GET", url, data=payload, headers=headers)
         response_dict_data = json.loads(str(response.text))
         return json.dumps(response_dict_data)
-
-
-
 
