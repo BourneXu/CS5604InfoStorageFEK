7bb8c956e4824fa4c81d9449f3130a2f09ef5eb1
Chao <chaoxu18@vt.edu>
2019-10-11 14:17:46 -0400

    Fix MySQL ip to service name

--- routes/dbconnect.py
+++ routes/dbconnect.py
@@ -6,7 +6,7 @@ import pymysql
 def connection():
     # TODO: configure mysql connection
 
-    host = "2001.0468.0c80.6102.0001.7015.41fc.29f6.ip6.name"
+    host = "mysql.cs5604-fek-db.svc"
     password = "3S4ecsEhMB"
     user = "root"
     database = "CS5604"
