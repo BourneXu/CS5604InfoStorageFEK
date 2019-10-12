19c98412690432f483213ca0105a05caa0307c83
Chao <chaoxu18@vt.edu>
2019-10-11 21:42:33 -0400

    Fix static/build

---
+++ static/build/precache-manifest.ccae5ddc71ebab449ffc067953bc5c34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cacb8cd1923170cb5e9924b21792d6c",
+    "url": "/static/build/index.html"
+  },
+  {
+    "revision": "9f9c078bac72a6e78821",
+    "url": "/static/build/static/css/main.5a338505.chunk.css"
+  },
+  {
+    "revision": "aa3417bf001570ab5b9e",
+    "url": "/static/build/static/js/2.0f55d3a1.chunk.js"
+  },
+  {
+    "revision": "9f9c078bac72a6e78821",
+    "url": "/static/build/static/js/main.a8dd4a0b.chunk.js"
+  },
+  {
+    "revision": "06d2ceab19ce7898dbf6",
+    "url": "/static/build/static/js/runtime-main.0ccc165f.js"
+  }
+]);
