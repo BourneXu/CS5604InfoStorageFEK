355f2f815cdbd6e9ece1cc25bc952936670c7a34
Chao <chaoxu18@vt.edu>
2019-10-13 16:22:38 -0400

    Update reactivesearch

--- reactivesearch/src/index.js
+++ reactivesearch/src/index.js
@@ -9,25 +9,43 @@ import {
     DataSearch,
     SelectedFilters
 } from "@appbaseio/reactivesearch";
-
+import axios from "axios";
 import "./styles.css";
 
 const { ResultListWrapper } = ReactiveList;
 
+const BASE_URI = 'http://0.0.0.0:3000';
+// const BASE_URI = 'http://2001.0468.0c80.6102.0001.7015.bf2d.eb25.ip6.name:3000'
+const client = axios.create({
+    baseURL: BASE_URI,
+    json: true
+});
+
 function App() {
     return (
         <ReactiveBase
-            app="shakespeare"
+            app="shakes"
             // credentials="egdxpZGTu:54c431d1-6a44-44b8-b84a-e46c4fed2de6"
-            url="http://2001.0468.0c80.6102.0001.7015.3fbb.aa59.ip6.name:9200/"
-            // TODO: configure this
-            // url="http://localhost:9200/"
+            // url="http://2001.0468.0c80.6102.0001.7015.3fbb.aa59.ip6.name:9200/"
+            url="http://localhost:9200/"
             theme={{
                 typography: {
                     fontFamily:
                         '"Lato", "Open Sans", "Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Noto Sans", "Ubuntu", "Droid Sans", "Helvetica Neue", sans-serif'
                 }
             }}
+            transformRequest={request => {
+                // console.log(request);
+                client({
+                    method: 'post',
+                    url: '/emitlogs',
+                    data: JSON.stringify(request),
+                    headers: {
+                        "Content-Type": "application/json",
+                    }
+                });
+                return request
+            }} //TODO: replace with apiClient function to send request to back-end (Flask API)
         >
             <div className="fek-searching">
                 <div className="searchbar">
@@ -38,13 +56,20 @@ function App() {
                         ]}
                         // fieldWeights={[2, 1, 2, 1, 1, 1, 1, 1]}
                         fuzziness={0}
+                        // debounce={100}
                         highlight={true}
                         highlightField={["text_entry"]}
-                        placeholder="Search books"
-                        title="Book Search"
+                        placeholder="Search ETD"
+                        title="FEK Searching System"
                         react={{
                             and: ['text_entry'],
                         }}
+                        renderNoSuggestion={() => (
+                            <div>
+                                No suggestions found
+                            </div>
+                        )}
+
                     />
                 </div>
 
@@ -76,7 +101,8 @@ function App() {
                             dataField="text_entry"
                             pagination={true}
                             className="result"
-                            size={10}
+                            size={5}
+                            loader="Loading Results.."
                             react={{
                                 and: ["filter_play_name", "filter_speech_number", "search"]
                             }}
@@ -84,19 +110,63 @@ function App() {
                                 <ResultListWrapper>
                                     {data.map(res => (
                                         <ResultList key={res._id}>
-                                            {/* <ResultList.Image src={res.speaker} /> */}
+                                            {/* <ResultList.Image src={res.image} /> */}
                                             <ResultList.Content>
                                                 <ResultList.Title>
-                                                    {res.play_name}
+                                                    <div
+                                                        className="book-title"
+                                                        dangerouslySetInnerHTML={{
+                                                            __html: res.play_name,
+                                                        }}
+                                                    />
                                                 </ResultList.Title>
                                                 <ResultList.Description>
-                                                    {res.text_entry}
+                                                    <div className="flex column justify-space-between">
+                                                        <div>
+                                                            <div>
+                                                                by{' '}
+                                                                <span className="authors-list">
+                                                                    {res.speaker}
+                                                                </span>
+                                                            </div>
+                                                            {/* <div className="ratings-list flex align-center">
+                                                                <span className="stars">
+                                                                    {Array(res.average_rating_rounded)
+                                                                        .fill('x')
+                                                                        .map((
+                                                                            res, // eslint-disable-line
+                                                                            index,
+                                                                        ) => (
+                                                                                <i
+                                                                                    className="fas fa-star"
+                                                                                    key={index} // eslint-disable-line
+                                                                                />
+                                                                            ))}
+                                                                </span>
+                                                                <span className="avg-rating">
+                                                                    ({res.average_rating} avg)
+														        </span>
+                                                            </div> */}
+                                                        </div>
+                                                        {/* <span className="pub-year">
+                                                            Pub {res.original_publication_year}
+                                                        </span> */}
+                                                        <div
+                                                            className="book-text"
+                                                            dangerouslySetInnerHTML={{
+                                                                __html: res.text_entry,
+                                                            }}
+                                                        />
+
+                                                    </div>
                                                 </ResultList.Description>
+
                                             </ResultList.Content>
                                         </ResultList>
                                     ))}
                                 </ResultListWrapper>
                             )}
+
                         />
                     </div>
                 </div>
