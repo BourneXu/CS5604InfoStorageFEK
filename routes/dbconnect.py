3e017f51ddc16a282074e56371117115ddb6c0f7
yanshen <yansh93@vt.edu>
2019-10-15 20:52:58 -0400

    log filter

--- routes/dbconnect.py
+++ routes/dbconnect.py
@@ -5,7 +5,7 @@ import pymysql
 
 def connection():
 
-    conn = pymysql.connect("localhost","root","Autumn","CS5604" )
+    conn = pymysql.connect("localhost","root","CSD@mysql-1872","CS5604" )
 
     c = conn.cursor()
     return c, conn
