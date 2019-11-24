e04be14f8bff8a23877051d29a99af1c7079704a
Edward Powell <edwardp9@vt.edu>
2019-11-24 12:29:13 -0500

    Admin has CRUD capabilities

--- routes/login.py
+++ routes/login.py
@@ -1,3 +1,4 @@
+import pymysql
 from routes.dbconnect import connection
 from flask import (
     Flask,
@@ -20,14 +21,269 @@ import time
 user_blueprint = Blueprint("user", __name__, template_folder="templates")
 
 
+
+
+
 @user_blueprint.route("/")
 def homepage():
     return render_template("index.html", error=error)
 
 
+@user_blueprint.route("/delete_user/" , methods=["GET", "POST"])
+def delete_user():
+    if request.method == "POST":
+        c, conn = connection()
+        username = request.form["username"]
+
+        data = c.execute(
+            "Delete from users WHERE username = (%s)", (username)
+        )
+
+    conn.commit()
+    c.close()
+    conn.close()
+    gc.collect()
+
+
+    return redirect(url_for("user.admin"))
+
+class ChangeEmailForm(Form):
+    newemail = TextField("Email", [validators.Length(min=6, max=50), validators.EqualTo("newemail2")])
+    newemail2 = TextField("Repeat Email", [validators.Length(min=6, max=50)])
+
+@user_blueprint.route("/change_email/" , methods=["GET", "POST"])
+def change_email():
+    try:
+        c, conn = connection()
+        error = ""
+        form = ChangeEmailForm(request.form)
+        username = request.form["username"]
+        newemail = request.form["newemail"]
+        oldemail = request.form["oldemail"]
+        UserInfo = []
+
+
+
+        if not form.validate():
+            print("I was here")
+            error = "Email must be between 6 and 50 characters and must match each other"
+            flash("Email must be between 6 and 50 characters and must match each other")
+            data3 = c.execute(
+                "SELECT * FROM users WHERE username = (%s)", (username)
+            )
+
+            data3 = c.fetchone()
+
+            UserInfo.append(data3[1]) #username
+            UserInfo.append(data3[3]) #email
+            UserInfo.append(data3[4]) #registration time
+            UserInfo.append(data3[5]) #user_type/access level
+            return render_template("admin_users.html", form=form, error=error, username=username, userinfo = UserInfo)
+
+        if request.method == "POST":
+
+            x = c.execute("SELECT * FROM users WHERE email = (%s)", (newemail))
+
+            if int(x) > 0:
+                error = "That email is already taken, please choose another."
+                flash("That email is already taken, please choose another.")
+                data3 = c.execute(
+                    "SELECT * FROM users WHERE username = (%s)", (username)
+                )
+                UserInfo = []
+                data3 = c.fetchone()
+
+                UserInfo.append(data3[1]) #username
+                UserInfo.append(data3[3]) #email
+                UserInfo.append(data3[4]) #registration time
+                UserInfo.append(data3[5]) #user_type/access level
+                return render_template("admin_users.html", form=form, error=error, username=username, userinfo = UserInfo)
+
+
+
+        data = c.execute(
+            "Update users set email = (%s) WHERE email = (%s)", (newemail, oldemail)
+        )
+
+        data2 = c.execute(
+            "SELECT * FROM users WHERE username = (%s)", (username)
+        )
+        UserInfo = []
+        data2 = c.fetchone()
+
+        UserInfo.append(data2[1]) #username
+        UserInfo.append(data2[3]) #email
+        UserInfo.append(data2[4]) #registration time
+        UserInfo.append(data2[5]) #user_type/access level
+
+
+
+        conn.commit()
+        c.close()
+        conn.close()
+        gc.collect()
+
+
+        return render_template("admin_users.html", username=username, userinfo = UserInfo)
+
+    except Exception as e:
+        return str(e)
+
+
+class ChangeUsernameForm(Form):
+    newusername = TextField("Username", [validators.Length(min=4, max=20), validators.EqualTo("newusername2")])
+    newusername2 = TextField("Repeat Username", [validators.Length(min=4, max=20)])
+
+
+
+
+@user_blueprint.route("/change_username/" , methods=["GET", "POST"])
+def change_username():
+    try:
+        c, conn = connection()
+        error = ""
+        form = ChangeUsernameForm(request.form)
+        username = request.form["username"]
+        newusername = request.form["newusername"]
+        UserInfo = []
+
+        if not form.validate():
+            print("I was here")
+            error = "Username must be between 5 and 20 characters and must match each other"
+            flash("Username must be between 5 and 20 characters and must match each other")
+            data3 = c.execute(
+                "SELECT * FROM users WHERE username = (%s)", (username)
+            )
+
+            data3 = c.fetchone()
+
+            UserInfo.append(data3[1]) #username
+            UserInfo.append(data3[3]) #email
+            UserInfo.append(data3[4]) #registration time
+            UserInfo.append(data3[5]) #user_type/access level
+            return render_template("admin_users.html", form=form, error=error, username=username, userinfo = UserInfo)
+
+
+        if request.method == "POST":
+
+            x = c.execute("SELECT * FROM users WHERE username = (%s)", (newusername))
+
+            if int(x) > 0:
+                error = "That username is already taken, please choose another."
+                flash("That username is already taken, please choose another.")
+                data3 = c.execute(
+                    "SELECT * FROM users WHERE username = (%s)", (username)
+                )
+
+                data3 = c.fetchone()
+
+                UserInfo.append(data3[1]) #username
+                UserInfo.append(data3[3]) #email
+                UserInfo.append(data3[4]) #registration time
+                UserInfo.append(data3[5]) #user_type/access level
+                return render_template("admin_users.html", form=form, error=error, username=username, userinfo = UserInfo)
+
+
+
+            data = c.execute(
+                "Update users set username = (%s) WHERE username = (%s)", (newusername,username)
+            )
+
+            data2 = c.execute(
+                "SELECT * FROM users WHERE username = (%s)", (newusername)
+            )
+            UserInfo = []
+            data2 = c.fetchone()
+
+            UserInfo.append(data2[1]) #username
+            UserInfo.append(data2[3]) #email
+            UserInfo.append(data2[4]) #registration time
+            UserInfo.append(data2[5]) #user_type/access level
+
+            conn.commit()
+            c.close()
+            conn.close()
+            gc.collect()
+
+        return render_template("admin_users.html", username=newusername, userinfo = UserInfo)
+
+    except Exception as e:
+        return str(e)
+
+
+
+@user_blueprint.route("/change_user_type/" , methods=["GET", "POST"])
+def change_user_type():
+    if request.method == "POST":
+        UserInfo = []
+        c, conn = connection()
+        username = request.form["username"]
+        data = c.execute(
+            "Update users set user_type = (%s) WHERE username = (%s)", ("admin",username)
+        )
+        data2 = c.execute(
+            "SELECT * FROM users WHERE username = (%s)", (username)
+        )
+
+        data2 = c.fetchone()
+
+        UserInfo.append(data2[1]) #username
+        UserInfo.append(data2[3]) #email
+        UserInfo.append(data2[4]) #registration time
+        UserInfo.append(data2[5]) #user_type/access level
+    conn.commit()
+    c.close()
+    conn.close()
+    gc.collect()
+
+
+    return render_template("admin_users.html", username=username, userinfo = UserInfo)
+
+    # return ('', 204)
+
+
+
+@user_blueprint.route("/admin_users/" , methods=["GET", "POST"])
+def admin_users():
+
+    if request.method == "POST":
+        UserInfo = []
+        username = request.form["username"]
+        c, conn = connection()
+        data = c.execute(
+            "SELECT * FROM users WHERE username = (%s)", (username)
+        )
+
+        data = c.fetchone()
+
+        UserInfo.append(data[1]) #username
+        UserInfo.append(data[3]) #email
+        UserInfo.append(data[4]) #registration time
+        UserInfo.append(data[5]) #user_type/access level
+
+
+    return render_template("admin_users.html", username=username, userinfo = UserInfo)
+
+
 @user_blueprint.route("/admin/")
 def admin():
-    return render_template("/admin.html")
+    Users = [];
+    c, conn = connection();
+    data = c.execute("SELECT * FROM users");
+
+
+    c = pymysql.cursors.DictCursor(conn);
+
+    query = "SELECT username FROM users";
+    c.execute(query)
+    rows = c.fetchall()
+    for row in rows:
+        for key, value in row.items():
+            Users.append(value)
+
+
+
+    return render_template("/admin.html", Users = Users, len = len(Users))
 
 
 @user_blueprint.route("/qa/")
@@ -108,6 +364,8 @@ class RegistrationForm(Form):
         "I accept the Terms and Conditions (updated Nov 10, 2019)",
         [validators.Required()],
     )
+
+
 
 
 @user_blueprint.route("/register/", methods=["GET", "POST"])
